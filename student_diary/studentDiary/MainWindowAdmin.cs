d6dc6294740b25a39f02be844d032ebaeaf65066
KelmurzinDanil <143899591+KelmurzinDanil@users.noreply.github.com>
2024-03-30 20:21:08 +0300

    Add files via upload

---
+++ student_diary/studentDiary/MainWindowAdmin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace studentDiary
+{
+    public partial class MainWindowAdmin : Form
+    {
+        public MainWindowAdmin()
+        {
+            MaximizeBox = false;
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(Ticker);
+        }
+        private void Ticker(object sender, EventArgs e)
+        {
+            Time.Text = DateTime.Now.ToShortTimeString();
+        }
+
+        private void AdminProfileButton_Click(object sender, EventArgs e)
+        {
+            AdministratorProfile administratorProfile = new AdministratorProfile();
+            administratorProfile.Show();
+        }
+
+        private void ScheduleAdminProfile_Click(object sender, EventArgs e)
+        {
+            AdminSchedule adminSchedule = new AdminSchedule();
+            adminSchedule.Show();
+        }
+
+        private void GroupList_Click(object sender, EventArgs e)
+        {
+            EditGroupForm editGroupForm = new EditGroupForm();
+            editGroupForm.Show();
+        }
+    }
+}
---
+++ student_diary/studentDiary/MainWindowStudent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace studentDiary
+{
+    public partial class MainWindowStudent : Form
+    {
+        public MainWindowStudent()
+        {
+            InitializeComponent();
+            Timer.Start();
+            MaximizeBox = false;
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(Ticker);
+        }
+        private void Ticker(object sender, EventArgs e)
+        {
+            Time.Text = DateTime.Now.ToShortTimeString();
+        }
+
+
+        private void MainWindowButton_Click(object sender, EventArgs e)
+        {
+            MainWindowStudent mainWindowStudent = new MainWindowStudent();
+            mainWindowStudent.Show();
+        }
+
+        private void StudentProfileButton_Click(object sender, EventArgs e)
+        {
+            StudentProfile studentProfile = new StudentProfile();
+            studentProfile.Show();
+        }
+
+        private void ScheduleStudentProfile_Click(object sender, EventArgs e)
+        {
+            StudentScheduleWindow studentScheduleWindow = new StudentScheduleWindow();
+            studentScheduleWindow.Show();
+        }
+    }
+}
